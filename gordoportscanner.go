6963a8e454eb037346de7fe23e186b0af57b8e60
BenPapple <benjamingeweke@googlemail.com>
2024-05-14 13:34:39 +0200

    Make nr of concurrent scans adjustable

--- gordoportscanner.go
+++ gordoportscanner.go
@@ -12,31 +12,33 @@ import (
 	"time"
 )
 
+// Flags
+var v = flag.Bool("v", false, "enable verbose output")
+var w = flag.Int("w", 100, "set worker count > 0")
+var t = flag.String("t", "localhost", "set target IP/URL")
+
 var workers int
 var openports = []int{}
 var host string = ""
 var isverbose bool
+var tokens chan struct{}
 
-// Flags
-var v = flag.Bool("v", false, "enable verbose output")
-var w = flag.Int("w", 10, "set worker count > 0")
-var t = flag.String("t", "localhost", "set target IP/URL")
+//var tokens = make(chan struct{}, *w)
 
 // Program start
 func main() {
-	//work := make(chan int, workers)
 	var wg sync.WaitGroup
+	tokens = make(chan struct{}, *w)
 	startTime := time.Now()
 
 	// Check for empty argument list
-	//fmt.Println("Args: ", os.Args)
 	if len(os.Args) <= 1 {
 		prheader()
 		os.Exit(0)
 	}
 
 	// Scanning system ports 1 to 1023
-	for i := 1; i < 1024; i++ {
+	for i := 1; i <= 65535; i++ {
 		wg.Add(1)
 		go scan(host, i, &wg)
 	}
@@ -46,8 +48,9 @@ func main() {
 	sort.Ints(openports)
 	fmt.Println("Final list of open ports: ", openports)
 	stopTime := time.Now()
-	duration := stopTime.Sub(startTime)
+
 	if isverbose {
+		duration := stopTime.Sub(startTime)
 		fmt.Println("Scan duration: ", duration)
 	}
 }
@@ -55,12 +58,15 @@ func main() {
 // Port scan logic
 func scan(host string, port int, wg *sync.WaitGroup) {
 	defer wg.Done()
+	tokens <- struct{}{}
 	target := fmt.Sprintf("%s:%d", host, port)
 	conn, err := net.Dial("tcp", target)
 	if err != nil {
+		<-tokens
 		return
 	}
 	conn.Close()
+	<-tokens
 	openports = append(openports, port)
 }
 
@@ -79,7 +85,7 @@ func init() {
 
 	} else {
 		// Default on negative input
-		workers = 10
+		workers = 100
 	}
 
 	if isverbose {
@@ -90,12 +96,16 @@ func init() {
 	checkIP := net.ParseIP(*t)
 	_, err := url.ParseRequestURI(*t)
 	if err != nil && *t != "localhost" && checkIP == nil {
-		panic(err)
+		prheader()
+		fmt.Println("Error: No valid IP or URI given")
+		fmt.Println("Check IP: ", checkIP)
+		fmt.Println("Target candidate1: ", *t)
+		os.Exit(0)
 	}
 
 	host = *t
 	if isverbose {
-		fmt.Println("Target chosen: ", host)
+		fmt.Println("Scan target chosen: ", host)
 	}
 }
 
